8bc5241fe2f7aa29d233e02d18dd4510b5f57f38
piumenotechnology <153091153+piumenotechnology@users.noreply.github.com>
2023-12-22 22:49:52 +0800

    Update VehiclePerformance.vue

--- src/views/app/performance/VehiclePerformance.vue
+++ src/views/app/performance/VehiclePerformance.vue
@@ -89,7 +89,7 @@
     <b-row>
       <b-colxx xxs="12" class="mb-4">
         <b-card :title="$t('performance.top-hired')">
-          <datatable-heading :title="$t('performance.top-hired')" :changePageSize="changePageSize" 
+          <datatable-heading :title="$t('performance.contract-history')" :changePageSize="changePageSize" 
           :searchChange="searchChange" :from="from" :to="to" :total="total" :perPage="perPage" :separator="true" noBreadcrumbs />
           <vuetable ref="vuetable" class="responsive-table" :api-url="apiBase" :query-params="makeQueryParams"
             :per-page="perPage" :reactive-api-url="true" :fields="fields" data-path="data.data" pagination-path="data"
@@ -258,6 +258,10 @@ export default {
         }).catch(_error => {
           console.log(_error)
         })
+    },
+    searchChange(val) {
+      this.search = val;
+      this.$refs.vuetable.refresh();
     },
     async fetchCars() {
       let url = apiUrl + "/purchaseorder";
