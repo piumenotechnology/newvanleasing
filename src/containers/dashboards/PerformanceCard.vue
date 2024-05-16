8c1adb72d81311e3f510011e5342050d73202839
piumenotechnology <153091153+piumenotechnology@users.noreply.github.com>
2024-05-16 13:01:58 +0800

    Update PerformanceCard.vue

--- src/containers/dashboards/PerformanceCard.vue
+++ src/containers/dashboards/PerformanceCard.vue
@@ -244,13 +244,10 @@ export default {
     actualIncome() {
       // const ongoing = this.getMonthDifference(new Date(this.vehicle.contract_start_date), new Date()) -1
       const ongoing = this.getMonthDifference(new Date(this.vehicle.contract_start_date), new Date())
-      let repayment = this.vehicle.monthly_payment * this.vehicle.hp_term
       // console.log(`month: ${ongoing} ${this.subTotal} `);
-      if(this.vehicle.purchase_method == 'Rent/Return') {
-        return (ongoing <= this.vehicle.term_months) ? ongoing * this.vehicle.monthly_payment + this.subTotal : this.theIncome - this.residualValue
-      } else {
-        return (ongoing <= this.vehicle.term_months) ? ongoing * this.vehicle.monthly_rental + this.vehicle.first_payment + this.subTotal : this.theIncome - this.residualValue
-      }
+      
+      return (ongoing <= this.vehicle.term_months) ? ongoing * this.vehicle.monthly_rental + this.vehicle.first_payment + this.subTotal
+      : this.theIncome - this.residualValue
     },
     actualCost() {
       let v = this.vehicle
