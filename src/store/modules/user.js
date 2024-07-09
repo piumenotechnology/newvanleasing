e657753041bb5f51d8b4e4dc98f9cb724ae5242a
piumenotechnology <153091153+piumenotechnology@users.noreply.github.com>
2024-07-09 23:20:33 +0800

    Update user.js

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -58,13 +58,18 @@ export default {
   },
   actions: {
     login({ commit }, payload) {
+      let config = {
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+        }
+      }
       commit('clearError')
       commit('setProcessing', true)
       axios
         .post(apiUrl + "/login", {
           username: payload.username,
           password: payload.password
-        })
+        }, config)
         .then(res => res.data)
         .then(
           user => {
