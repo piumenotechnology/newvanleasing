d92c66d0e3d311106799d79c3fa0d9f23a7ae045
piumenotechnology <153091153+piumenotechnology@users.noreply.github.com>
2024-07-16 04:44:37 +0800

    Update config.js
    chaged from apivl.piumenosystem.com

--- src/constants/config.js
+++ src/constants/config.js
@@ -5,7 +5,7 @@ export const adminRoot = '/app';
 export const searchPath = `${adminRoot}/#`
 export const buyUrl = 'https://1.envato.market/nEyZa'
 // export const apiUrl = 'http://vanleasing-frontend.test/api';
-export const apiUrl = 'https://apivl.piumenosystem.com/api';
+export const apiUrl = 'https://red-shark-302941.hostingersite.com/api';
 
 export const subHiddenBreakpoint = 1440
 export const menuHiddenBreakpoint = 768
