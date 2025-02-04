9bd0486464dd862f27a78a4fb72e23dd37831bab
piumenotechnology <153091153+piumenotechnology@users.noreply.github.com>
2025-02-05 04:51:16 +0800

    Update PerformanceDetail.vue

--- src/views/app/performance/PerformanceDetail.vue
+++ src/views/app/performance/PerformanceDetail.vue
@@ -1,102 +1,301 @@
 <template>
-  <div v-if="isLoading" class="loading" key="itemLoading"></div>
-  <div v-else>
-    <b-row class="mb-3">
-      <b-colxx xxs="12">
-        <b-link @click.prevent="$router.go(-1)" class="heading-icon baseline">
-          <i class="simple-icon-arrow-left" />
-        </b-link>
-        <piaf-breadcrumb :heading="vehicle.vehicle_registration" />
-        <div class="top-right-button-container">
-          <h4 class="d-flex align-items-center">
-            <span class="text-muted font-weight-light text-small">{{ $t("pages.status") }}</span>
-            <b-badge v-if="vehicle.status_next_step == 'Available'" pill variant="success"
-              class="ml-2">{{ vehicle.status_next_step }}</b-badge>
-            <b-badge v-else-if="vehicle.status_next_step == 'Hired'" pill variant="primary"
-              class="ml-2">{{ vehicle.status_next_step }}</b-badge>
-            <b-badge v-else pill variant="dark"
-              class="ml-2">{{ vehicle.status_next_step }}</b-badge>
-          </h4>
-        </div>
-        <!-- <div class="separator mb-5"></div> -->
+  <div>
+    <b-row class="icon-cards-row invert d-flex justify-content-stretch mb-4">
+      <b-colxx xxs="12" class="px-3">
+        <h3 v-if="vehicle.next_step_status_sales == 'Sold'" class="list-heading mb-3">Actual Value</h3>
+        <h3 v-else class="list-heading mb-3">Projected Performance</h3>
+      </b-colxx>
+      <b-colxx>
+        <icon-card
+          :title="$t('performance.income')"
+          icon="iconsminds-financial"
+          :isComa="true"
+          :isMoney="true"
+          :value="Number(theIncome)"
+        />
+      </b-colxx>
+      <b-colxx>
+        <icon-card
+          :title="$t('performance.cost')"
+          icon="iconsminds-billing"
+          :isComa="true"
+          :isMoney="true"
+          :value="Number(theCost)"
+        />
+      </b-colxx>
+      <b-colxx>
+        <icon-card
+          :title="$t('performance.margin')"
+          icon="iconsminds-scale"
+          :isComa="true"
+          :isMoney="true"
+          :value="Number(theMargin)"
+        >
+        <h6 class="position-absolute font-weight-normal card-top-buttons text-white">
+          <b-badge v-show="isComputed && theMargin > 0" pill id="annual-margin">{{annualMargin}}%</b-badge>
+          <b-tooltip target="annual-margin" placement="bottom" :title="$t('performance.tooltip-margin')"/>
+        </h6>
+        </icon-card>
+      </b-colxx>
+      <b-colxx v-show="!isSold">
+        <icon-card
+          :title="$t('performance.residual')"
+          icon="iconsminds-money-bag"
+          :isComa="true"
+          :isMoney="true"
+          :value="Number(residualValue)"
+        />
+      </b-colxx>
+      <b-colxx v-show="isSold">
+        <icon-card
+          :title="$t('performance.sold-price')"
+          icon="iconsminds-pricing"
+          :isComa="true"
+          :isMoney="true"
+          :value="Number(soldPrice)"
+        />
+      </b-colxx>
+      <b-colxx>
+        <icon-card
+          :title="$t('performance.rental-income')"
+          icon="iconsminds-pricing"
+          :isComa="true"
+          :isMoney="true"
+          :value="Number(rentalIncome)"
+        />
       </b-colxx>
     </b-row>
-    <b-row>
-      <b-colxx xxs="12" lg="12">
-        <performance-card :vehicle="vehicle" :subTotal="subTotal" />
+    <b-row v-if="vehicle.next_step_status_sales !== 'Sold'" class="icon-cards-row d-flex justify-content-stretch mb-4">
+      <b-colxx xxs="12" class="px-3">
+        <h3 class="list-heading mb-3">Current Performance (today)</h3>
       </b-colxx>
-      <b-colxx xxs="12" lg="5">
-        <b-row>
-          <b-colxx xxs="6" class="mb-4">
-            <b-card class="mb-lg-3" no-body>
-              <vehicle-detail-card v-if="vehicle" :vehicle="vehicle" />
-            </b-card>
-          </b-colxx>
-          <b-colxx xxs="6" class="mb-4">
-            <b-card class="mb-lg-3" no-body>
-              <vehicle-cost-card v-if="vehicle" :vehicle="vehicle" />
-            </b-card>
-          </b-colxx>
-        </b-row>
+      <b-colxx>
+        <icon-card
+          :title="$t('performance.income')"
+          icon="iconsminds-financial"
+          :isComa="true"
+          :isMoney="true"
+          :value="Number(actualIncome)"
+        />
       </b-colxx>
-      <b-colxx xxs="12" lg="7" class="mb-4">
-        <b-card class="mb-lg-3" no-body>
-          <contract-history-card v-if="vehicle" :id="$route.params.id" />
-        </b-card>
+      <b-colxx>
+        <icon-card
+          :title="$t('performance.cost')"
+          icon="iconsminds-billing"
+          :isComa="true"
+          :isMoney="true"
+          :value="Number(actualCost)"
+        />
       </b-colxx>
+      <b-colxx>
+        <icon-card
+          :title="$t('performance.margin')"
+          icon="iconsminds-scale"
+          :isComa="true"
+          :isMoney="true"
+          :value="Number(actualMargin)"
+        >
+          <h6 class="position-absolute font-weight-normal card-top-buttons text-white">
+            <b-badge v-show="isComputed && actualMargin > 0" pill id="annual-actualmargin">{{marginPercentage}}%</b-badge>
+            <b-tooltip target="annual-actualmargin" placement="bottom" :title="$t('performance.tooltip-margin-2')"/>
+          </h6>
+        </icon-card>
+      </b-colxx>
+      <b-colxx v-show="!isSold">
+        <icon-card
+          :title="$t('performance.residual')"
+          icon="iconsminds-money-bag"
+          :isComa="true"
+          :isMoney="true"
+          :value="Number(residualValue)"
+        />
+      </b-colxx>
+      <b-colxx v-show="isSold">
+        <icon-card
+          :title="$t('performance.sold-price')"
+          icon="iconsminds-pricing"
+          :isComa="true"
+          :isMoney="true"
+          :value="Number(soldPrice)"
+        />
+      </b-colxx>
+
     </b-row>
   </div>
 </template>
 <script>
 import axios from 'axios';
-import { apiUrl } from '../../../constants/config';
-import VehicleDetailCard from '../../../containers/dashboards/VehicleDetailCard';
-import VehicleCostCard from '../../../containers/dashboards/VehicleCostCard';
-import PerformanceCard from '../../../containers/dashboards/PerformanceCard';
-import ContractHistoryCard from '../../../containers/dashboards/ContractHistoryCard';
-
+import { apiUrl } from '../../constants/config';
+import IconCard from '../../components/Cards/IconCard';
 export default {
   components: {
-    'vehicle-detail-card': VehicleDetailCard,
-    'vehicle-cost-card': VehicleCostCard,
-    'performance-card': PerformanceCard,
-    'contract-history-card': ContractHistoryCard
+      'icon-card': IconCard
   },
+  props: ['vehicle', 'subTotal'],
   data() {
-  return {
-    isLoading: true,
-    subTotal: 0,
-    vehicle: [],
-    report: []
-  }
+    return {
+      baseInterest: 0,
+      totalIncome: 0,
+      otherIncome: 0,
+      totalCost: 0,
+      otherCost: 0,
+      residualValue: 0,
+      rentalIncome: 0,
+      soldPrice: 0,
+      activeSales: '',
+      isSold: false,
+      isComputed: false
+    }
   },
   methods: {
     getSum(total, num) {
       return total + Math.round(num);
     },
-    fetchCar(id) {
-      let url = apiUrl + "/listvehicleinvehiclecard/" + id
+    getMonthDifference(startDate, endDate) {
+      let outstanding = endDate.getMonth() -
+          startDate.getMonth() +
+          12 * (endDate.getFullYear() - startDate.getFullYear());
+      return outstanding
+    },
+    async getBaseInterest(id) {
+      let url = apiUrl + "/sumtotalbaseinterest/" + id
       axios
       .get(url)
         .then(r => r.data)
         .then(res => {
-          this.isLoading = false
-          let subTotal = res.data.filter(x => x.next_step_status_sales !== "Hired").map(x => x.total_income)
-          // console.log(subTotal);
-          if(subTotal.length > 0) {
-            this.subTotal = subTotal.reduce(this.getSum, 0)
-          } else {
-            this.subTotal = 0
-          }
-          this.vehicle = res.data.pop()
+          this.baseInterest = (res.data.sum_total_base_interest == null) ? 0 : res.data.sum_total_base_interest
         })
-        .catch(err => {
-          console.log(err.message)
+    },
+    async getTotalIncome(id) {
+      let url = apiUrl + "/listtotalincome/" + id
+      axios
+      .get(url)
+        .then(r => r.data)
+        .then(res => {
+          this.totalIncome = res.data.sum_total_income
+        })
+    },
+    async getOtherIncome(id) {
+      let url = apiUrl + "/listotherincome/" + id
+      axios
+      .get(url)
+        .then(r => r.data)
+        .then(res => {
+          this.otherIncome = res.data.sum_other_income
+        })
+    },
+    async getTotalCost(id) {
+      let url = apiUrl + "/listtotalcost/" + id
+      axios
+      .get(url)
+        .then(r => r.data)
+        .then(res => {
+          this.totalCost = res.data.sum_total_cost
+        })
+    },
+    async getOtherCost(id) {
+      let url = apiUrl + "/listothercost/" + id
+      axios
+      .get(url)
+        .then(r => r.data)
+        .then(res => {
+          this.otherCost = res.data.sum_other_cost
+        })
+    },
+    async getResidualValue(id) {
+      let url = apiUrl + "/listresidualvalue/" + id
+      axios
+      .get(url)
+        .then(r => r.data)
+        .then(res => {
+          this.residualValue = (res.data.sum_residual_value == null) ? 0 : res.data.sum_residual_value
+        })
+    },
+    async getRentalIncome(id) {
+      let url = apiUrl + "/listrentalincome/" + id
+      axios
+      .get(url)
+        .then(r => r.data)
+        .then(res => {
+          this.rentalIncome = (res.data.sum_rental_income == null) ? 0 : res.data.sum_rental_income
+        })
+    },
+    async getSoldPrice(id) {
+      let url = apiUrl + "/listsoldprice/" + id
+      axios
+      .get(url)
+        .then(r => r.data)
+        .then(res => {
+          if(res.data.sum_sold_price !== null) {
+            this.isSold = true
+            this.soldPrice = res.data.sum_sold_price
+          } else {
+            this.soldPrice = 0
+          }
         })
     }
   },
+  computed: {
+    theIncome() {
+      if(this.isSold == true) {
+        return (this.otherIncome !== null) ? Math.abs(Number(this.rentalIncome) + Number(this.soldPrice) + Number(this.otherIncome))
+        : Math.abs(Number(this.rentalIncome) + Number(this.soldPrice))
+      } else {
+        return (this.otherIncome !== null) ? Math.abs(Number(this.rentalIncome) + Number(this.otherIncome) + Number(this.residualValue))
+        : Math.abs(Number(this.rentalIncome) + Number(this.residualValue))
+      }
+    },
+    theCost() {
+      if(this.vehicle.purchase_method == "Cash") {
+        return Number(this.vehicle.price_otr)
+      } else {
+        return (this.otherCost !== null) ? Math.abs(Number(this.totalCost) + Number(this.otherCost) + Number(this.baseInterest)) : this.totalCost + this.baseInterest
+      }
+    },
+    theMargin() {
+      return Number(this.theIncome) - Number(this.theCost)
+    },
+    actualIncome() {
+      // const ongoing = this.getMonthDifference(new Date(this.vehicle.contract_start_date), new Date()) -1
+      const ongoing = this.getMonthDifference(new Date(this.vehicle.contract_start_date), new Date())
+      // console.log(`month: ${ongoing} ${this.subTotal} `);
+
+      return (ongoing <= this.vehicle.term_months) ? ongoing * this.vehicle.monthly_rental + this.vehicle.first_payment + this.subTotal
+      : this.theIncome - this.residualValue
+    },
+    actualCost() {
+      let v = this.vehicle
+      // let interest = 1 + (v.hp_interest_per_annum / 100)
+      // let subTotal = v.monthly_payment * interest
+      let subTotal = v.regular_monthly_payment + v.vehicle_tracking
+
+      const ongoing = this.getMonthDifference(new Date(v.hire_purchase_starting_date), new Date())
+      console.log(ongoing);
+      return (ongoing > 0 && ongoing < v.hp_term) ? (ongoing * subTotal) + v.hp_deposit_amount + this.baseInterest : this.theCost
+    },
+    actualMargin() {
+      return this.actualIncome - this.actualCost
+    },
+    annualMargin() {
+      return parseFloat(this.theMargin / this.theIncome * 100).toFixed(2);
+    },
+    marginPercentage() {
+      return parseFloat(this.actualMargin / this.actualIncome * 100).toFixed(2);
+    }
+  },
   mounted() {
-    this.fetchCar(this.$route.params.id)
+    this.getBaseInterest(this.$route.params.id)
+    this.getTotalIncome(this.$route.params.id)
+    this.getOtherIncome(this.$route.params.id)
+    this.getTotalCost(this.$route.params.id)
+    this.getOtherCost(this.$route.params.id)
+    this.getResidualValue(this.$route.params.id)
+    this.getRentalIncome(this.$route.params.id)
+    this.getSoldPrice(this.$route.params.id)
+    setTimeout(() => {
+      this.isComputed = true
+    }, 1500);
+    // this.getActiveSales(this.vehicle.id)
   }
 }
 </script>
