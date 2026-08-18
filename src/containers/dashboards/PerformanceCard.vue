be6665fea6096d9a057489f5f9200b9970bef621
piumenotechnology <153091153+piumenotechnology@users.noreply.github.com>
2026-08-18 21:06:33 +0800

    Update PerformanceCard.vue

--- src/containers/dashboards/PerformanceCard.vue
+++ src/containers/dashboards/PerformanceCard.vue
@@ -105,7 +105,7 @@
       <b-colxx>
         <icon-card
           :title="$t('performance.current-settlement')"
-          icon="iconsminds-financial"
+          icon="iconsminds-receipt-3"
           :isComa="true"
           :isMoney="true"
           :value="Number(estCurrentSettlement)"
